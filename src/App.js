092e1ed98ae541e85e82eddc9105cce0d9987eb7
Estellio <pirko.vichmann@voco.ee>
2023-04-15 16:13:49 +0300

    3. Komponendi sisustamine andmetega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+    {
+        date: new Date(2023, 0, 10),
+        title: 'New book',
+        price: 10.99
+    },
+    {
+        date: new Date(2023, 0, 10),
+        title: 'New shelf',
+        price: 99.99
+    }
+]
+
   return (
     <div className="App">
-        <ExpenseItem></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
+        <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
     </div>
   );
 }
