f74a228a4cbd82f4809154394836eb559006d3b7
Estellio <pirko.vichmann@voco.ee>
2023-04-16 19:45:49 +0300

    Andmete edastamine alamkomponendist ülemkomponendile (bottom-up)

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+    const saveExpenseDatahandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        console.log(expenseData)
+    }
+
     return(
         <div className='new-expense'>
-            <ExpenseForm></ExpenseForm>
+            <ExpenseForm onSaveExpenseData={saveExpenseDatahandler}></ExpenseForm>
         </div>
     )
 }
